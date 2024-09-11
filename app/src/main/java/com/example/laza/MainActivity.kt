8ba8521b49f4d1bbf0453d2a34021affe59e533a
ahmed.habib <ahmed.habib@sewedy.com.eg>
2024-09-11 17:47:35 +0300

    Added shared view model

--- app/src/main/java/com/example/laza/MainActivity.kt
+++ app/src/main/java/com/example/laza/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.laza
 
 import android.os.Bundle
-import android.view.View
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
-import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -15,89 +12,46 @@ import com.example.laza.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var navGraph: NavGraph
-    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
-
-    private val sharedViewModel: LazaSharedViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        init()
-        observer()
-
-        // Setup Navigation Drawer toggle with ActionBar
-        setupDrawerNavigation()
-
-        // Open Drawer on toolbar button click
-        setupToolbarButtonForDrawer()
+        onClick()
+        setUpBottomNav()
+        setupBurgerMenuButton()
     }
 
-    private fun observer() {
-        sharedViewModel.hideUnNecessaryView.observe(this) { hide ->
-            if (hide == true) {
-                binding.toolbar.visibility = View.GONE
-                binding.bottomNav.visibility = View.GONE
-            } else {
-                binding.toolbar.visibility = View.VISIBLE
-                binding.bottomNav.visibility = View.VISIBLE
-            }
-        }
-    }
-
-    private fun init() {
-        // Find the NavHostFragment and NavController
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.mainNavGraph) as NavHostFragment
-        navController = navHostFragment.navController
-        val navInflater = navController.navInflater
-        navGraph = navInflater.inflate(R.navigation.nav_graph)
-        navController.graph = navGraph
-
-        // Tie Bottom Navigation with NavController
-        binding.bottomNav.setupWithNavController(navController)
-    }
-
-    private fun setupDrawerNavigation() {
-        // Create AppBarConfiguration with the top-level destinations
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-
-            ), binding.drawerLayout
-        )
-
-        // Set up the ActionBar with NavController and DrawerLayout
-        setSupportActionBar(binding.toolbar)
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
-
-        // Set up the NavigationView with NavController
-        binding.navigationDrawer.setupWithNavController(navController)
-    }
-
-    private fun setupToolbarButtonForDrawer() {
-        binding.toolbar.setOnClickListener {
+    private fun onClick() {
+        binding.sideMenuId.setOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.START)
         }
     }
 
-    // Handle the ActionBar’s Up button behavior
+    private fun setUpBottomNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainNavGraph) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    private fun setupBurgerMenuButton() {
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.settingsFragment),
+            binding.drawerLayout
+        )
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(
             navController,
-            appBarConfiguration
+            binding.drawerLayout
         ) || super.onSupportNavigateUp()
     }
-
-    // Override back press to close drawer if open
-    override fun onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
 }
+
--- app/src/main/java/com/example/laza/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/laza/settings/SettingsFragment.kt
@@ -1,12 +1,12 @@
-package com.example.laza.sideMenu
+package com.example.laza.settings
 
 import android.os.Bundle
 import android.view.View
 import com.example.common.BaseFragment
-import com.example.laza.databinding.FragmentSideMenuBinding
+import com.example.laza.databinding.FragmentSettingsBinding
 
 
-class SideMenuFragment : BaseFragment<FragmentSideMenuBinding>(FragmentSideMenuBinding::inflate) {
+class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
